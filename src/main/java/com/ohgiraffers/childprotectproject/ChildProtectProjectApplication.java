ae119141264ececf8030110218d951fc573e101d
inredzero <caesete0524@naver.com>
2024-06-03 19:30:29 +0900

    first settings...

---
+++ src/main/java/com/ohgiraffers/childprotectproject/ChildProtectProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.childprotectproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildProtectProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildProtectProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/childprotectproject/ChildProtectProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.childprotectproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildProtectProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
